709be3bb415e1fcb89300f82f2bb52e566866565
Carlos_DominguezMartinez <da0134xh@go.minnstate.edu>
2025-02-04 07:17:16 +0000

    Part 2 Finished?

--- Module3Project/Module3Project.py
+++ Module3Project/Module3Project.py
@@ -3,13 +3,21 @@
 # Date: 01/29/2025
 
 # Store our ice screem shop's menu items
-flavors = ["vanilla", "caramel", "mint"]
+cones = ["cake", "sugar", "waffle"] # type of cone
+flavors = ["vanilla", "caramel", "mint", "strawberry", "oreo", "mango"] # Three new flavors added!
 toppings = ["sprinkles", "nuts", "cherry"]
 prices = {"scoop": 2.50, "topping": 0.50}
 
 def display_menu():
     """Shows available flavors and toppings to the customer"""
     print("\n=== Welcome to the Ice Cream Shop! ===")
+
+    print("\nAvailable cones:")
+
+    # Loop through the cone list and show each cone
+    for cone in cones:
+        print(f" - {cone}")
+
     print("\nAvailable flavors:")
 
     # Loop through the flavor list and show each flavor, then
@@ -26,6 +34,23 @@ def display_menu():
     print(f"Scoop: ${prices['scoop']:.2f} each")
     print(f"Toppings: ${prices['topping']:.2f} each")
 
+def get_cones():
+    """Gets cone option from the customer"""
+    cone_type = ""
+
+    # Use a while loop to ask user for type of cone
+    while True:
+        cone_option = input("\nWhat kind of cone would you like? ").lower()
+
+        # Will ask user until they pick available cone
+        if cone_option in cones:
+            cone_type = cone_option
+            break
+        print("Sorry, that is not a cone option.")
+
+    # return what type of cone they chose
+    return cone_type
+
 
 def get_flavors():
     """Gets ice cream flavor choices from the customer"""
@@ -35,7 +60,7 @@ def get_flavors():
     while True:
         try:
             # Prompt the user to choose their scoops of ice cream
-            num_scoops = int(input("\nHow many scoops would you like? (1-3)"))
+            num_scoops = int(input("\nHow many scoops would you like? (1-3) "))
             # Validate the input
             if 1 <= num_scoops <= 3:
                 break
@@ -44,7 +69,7 @@ def get_flavors():
             print("Please enter a number.")
 
     # Prompt the user to enter the ice cream flavor
-    print("\nFor each scoop, enter the flavor you'd like:")
+    print("\nFor each scoop, enter the flavor you'd like: ")
     for i in range(num_scoops): #For loop prompts for each scoop of ice cream
         # Nested while loop handles the user's input and validation
         while True:
@@ -82,13 +107,27 @@ def get_toppings():
 
 def calculate_total(num_scoops, num_toppings):
     """Calculates the total cost of the order"""
+
+    total_cost = 0.00
+
     scoop_cost = num_scoops * prices["scoop"]
     topping_cost = num_toppings * prices["topping"]
-    return scoop_cost + topping_cost
 
-def print_reciept(num_scoops, chosen_flavors, chosen_toppings):
+    #Calculate discount of 10% if over $10
+    subtotal = scoop_cost + topping_cost
+    if subtotal > 10.00:
+        total_cost = subtotal - (subtotal * .10)
+    else:
+        total_cost = subtotal
+
+    return total_cost
+
+def print_reciept(num_scoops, chosen_flavors, chosen_toppings, cone_type):
     """Prints a nice receipt for the customer"""
     print("\n== Your Ice Cream Order ===")
+
+    print("You chose a " + cone_type + " cone\n")
+
     for i in range(num_scoops):
         print(f"Scoop {i+1}: {chosen_flavors[i].title()}") #.title first letter will be capitalize
 
@@ -109,13 +148,15 @@ def print_reciept(num_scoops, chosen_flavors, chosen_toppings):
 # Main function - updated test function
 def main():
     display_menu()
+    # Call get cone fucntion, which returns the type of cone chosen
+    cone_type = get_cones()
     # Call get flavors function, which returns the number of scoops
     # and the list of flavors
     num_scoops, chosen_flavors = get_flavors()
     # Call the get toppings functions which returns the list of toppings
     chosen_toppings = get_toppings()
     # Display the reciepts
-    print_reciept(num_scoops, chosen_flavors, chosen_toppings)
+    print_reciept(num_scoops, chosen_flavors, chosen_toppings, cone_type)
 
     # Automatically execute the main function when the application runs
 if __name__ == "__main__":
